e3ed166036322b0b93ac9a47b951c994f0698275
Christian Calderon <christianqtc@yahoo.com>
2020-03-12 19:44:08 -0700

    Dark Mode toggle button and feature added.

--- tetris.js
+++ tetris.js
@@ -7,7 +7,7 @@ const scoreElement = document.getElementById("score");
 const ROW = 20; // size of how many squares in each row
 const COL = COLUMN = 10; // size of how many squares in each column
 const SQ = SQUARE = 20; // size of each square(in pixels)
-const VACANT = "BLACK"; // color of empty square
+let VACANT = "WHITE"; // color of empty square
 const ROWNEXT = 4;
 const COLNEXT = 4;
 const LINECOLOR = "GREY"
@@ -35,10 +35,13 @@ function drawSquareNext(x,y,color){
 // create the tetris board
 
 let board = [];
+let boardOneZero = [];
 for(r = 0; r < ROW; r++){
     board[r] = [];
+    boardOneZero[r] = [];
     for(c = 0; c < COL; c++){
         board[r][c] = VACANT;
+        boardOneZero[r][c] = 0;
     }
 }
 
@@ -56,7 +59,9 @@ for(r = 0; r < ROWNEXT; r++){
 function drawBoard(){
     for(r = 0; r < ROW; r++){
         for(c = 0; c < COL; c++){
+          if(boardOneZero[r][c] == 0){
             drawSquare(c,r,board[r][c]);
+          }
         }
     }
 }
@@ -106,8 +111,8 @@ let p = randomPiece();
 // initiate next up piece
 
 let p2 = randomPiece();
-p2.x = 0;
-p2.y = 0;
+p2.x = 3;
+p2.y = -2;
 drawNext();
 
 // draw the Object Piece
@@ -180,6 +185,17 @@ function clearBoardNext(){
   }
 }
 
+function clearBoard(){
+  for(r = 0; r < ROW; r++){
+      board[r] = [];
+      for(c = 0; c < COL; c++){
+        if(board[r][c] == "WHITE" || "BLACK" && boardOneZero[r][c] == 0){
+            board[r][c] = VACANT;
+        }
+      }
+  }
+}
+
 // move right the piece on right arrow key
 
 Piece.prototype.moveRight = function(){
@@ -228,6 +244,18 @@ Piece.prototype.rotate = function(){
 
 let score = 0
 
+// function zeros(dimensions) {
+//     var array = [];
+//
+//     for (var i = 0; i < dimensions[0]; ++i) {
+//         array.push(dimensions.length == 1 ? 0 : zeros(dimensions.slice(1)));
+//     }
+//     return array;
+// }
+// let boardOneZero[][] = zeros([ROW,COL]);
+
+
+
 Piece.prototype.lock = function(){
     for(r = 0; r < this.activeTetrimino.length; r++){
         for(c = 0; c < this.activeTetrimino.length; c++){
@@ -244,6 +272,7 @@ Piece.prototype.lock = function(){
             }
             // we lock the piece in place
             board[this.y+r][this.x+c] = this.color;
+            boardOneZero[this.y+r][this.x+c] = 1;
         }
     }
     // remove full rows or clear line
@@ -258,6 +287,7 @@ Piece.prototype.lock = function(){
             for(y = r; y > 1; y--){
                 for(c = 0; c < COL; c++){
                     board[y][c] = board[y-1][c];
+                    boardOneZero[y][c] = boardOneZero[y-1][c];
                 }
             }
             // the top row board [0][...] has no row above it
@@ -274,6 +304,67 @@ Piece.prototype.lock = function(){
     // update the score
     scoreElement.innerHTML = score;
 }
+
+// DARK MODE
+
+
+  // Get the checkbox
+  var checkBox = document.getElementById("darkCheck");
+  // Get the output text
+  var text = document.getElementById("text");
+
+  // If the checkbox is checked, display the output text
+  checkBox.addEventListener('change', function(){
+  if (checkBox.checked == true){
+    VACANT = "BLACK";
+    for(r = 0; r < ROW; r++){
+        board[r] = [];
+        for(c = 0; c < COL; c++){
+            board[r][c] = VACANT;
+        }
+    }
+    clearBoard();
+    drawBoard();
+    console.log(VACANT);
+
+// darkmode for next
+    for(r = 0; r < ROWNEXT; r++){
+        boardNext[r] = [];
+        for(c = 0; c < COLNEXT; c++){
+            boardNext[r][c] = VACANT;
+        }
+    }
+    clearBoardNext();
+    drawBoardNext();
+    drawNext();
+    console.log(VACANT);
+  } else {
+    VACANT = "WHITE";
+    for(r = 0; r < ROW; r++){
+        board[r] = [];
+        for(c = 0; c < COL; c++){
+            board[r][c] = VACANT;
+        }
+    }
+    clearBoard();
+    drawBoard();
+
+// darkmode for next
+
+    for(r = 0; r < ROWNEXT; r++){
+        boardNext[r] = [];
+        for(c = 0; c < COLNEXT; c++){
+            boardNext[r][c] = VACANT;
+        }
+    }
+    clearBoardNext();
+    drawBoardNext();
+    drawNext();
+    console.log("UNCHECKED");
+  }
+});
+
+
 
 // collision function
 
@@ -321,17 +412,23 @@ function drawNext(){
 
 document.addEventListener("keydown",CONTROL);
 
+// uses the arrows keys on the keyboard to control the game
 function CONTROL(event){
     if(event.keyCode == 37){
+        event.preventDefault(); // this command ignores using the arrows to
+                                // scroll up and down the browser
         p.moveLeft();
         dropStart = Date.now();
     } else if (event.keyCode == 38){
+        event.preventDefault();
         p.rotate();
         dropStart = Date.now();
     } else if (event.keyCode == 39){
+        event.preventDefault();
         p.moveRight();
         dropStart = Date.now();
     } else if (event.keyCode == 40){
+        event.preventDefault();
         p.moveDown();
         dropStart = Date.now();
     }
